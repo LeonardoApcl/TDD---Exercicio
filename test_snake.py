100d5e862a3818f8a124bbdde9499784a74e693a
Leonardo Apicelo Valente <leonardoapicelo.valente@gmail.com>
2026-04-16 01:30:39 -0300

    RED: testes movimento para baixo e para esquerda
    Para agilizar coloquei 2 testes de 1 vez

--- test_snake.py
+++ test_snake.py
@@ -35,3 +35,29 @@ def test_snake_move_up():
     assert player.body[0] == (5, 4)      # A cabeça deve ir para cima (y-1)
     assert len(player.body) == 2         # O tamanho deve continuar o mesmo
     assert player.body[-1] == (5, 5)     # O corpo antigo (4,5) sumiu, o novo é (5,5)
+
+#Teste (Red)
+def test_snake_move_down():
+    # Arrange
+    player = Snake(start_x=5, start_y=5) # Corpo inicial: [(5,5), (5,4)]
+    
+    # Act
+    player.move('s', max_x=10, max_y=10) 
+    
+    # Assert
+    assert player.body[0] == (5, 6)      # A cabeça deve ir para baixo (y+1)
+    assert len(player.body) == 2         # O tamanho deve continuar o mesmo
+    assert player.body[-1] == (5, 5)     # O corpo antigo (4,5) sumiu, o novo é (5,5)
+
+#Teste (Red)
+def test_snake_move_left():
+    # Arrange
+    player = Snake(start_x=5, start_y=5) # Corpo inicial: [(5,5), (5,4)]
+    
+    # Act
+    player.move('a', max_x=10, max_y=10) 
+    
+    # Assert
+    assert player.body[0] == (4, 5)      # A cabeça deve ir para a esquerda (x-1)
+    assert len(player.body) == 2         # O tamanho deve continuar o mesmo
+    assert player.body[-1] == (5, 5)     # O corpo antigo (4,5) sumiu, o novo é (5,5)
