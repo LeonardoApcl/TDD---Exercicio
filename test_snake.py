e5f893510e3445c25105b8611dff0b489fc376ea
Leonardo Apicelo Valente <leonardoapicelo.valente@gmail.com>
2026-04-16 01:26:58 -0300

    RED: teste movimento para cima

--- test_snake.py
+++ test_snake.py
@@ -22,3 +22,16 @@ def test_snake_move_right():
     assert player.body[0] == (6, 5)      # A cabeça deve ir para a direita (x+1)
     assert len(player.body) == 2         # O tamanho deve continuar o mesmo
     assert player.body[-1] == (5, 5)     # O corpo antigo (4,5) sumiu, o novo é (5,5)
+
+#Teste (Red)
+def test_snake_move_up():
+    # Arrange
+    player = Snake(start_x=5, start_y=5) # Corpo inicial: [(5,5), (5,4)]
+    
+    # Act
+    player.move('w', max_x=10, max_y=10) 
+    
+    # Assert
+    assert player.body[0] == (5, 4)      # A cabeça deve ir para cima (y-1)
+    assert len(player.body) == 2         # O tamanho deve continuar o mesmo
+    assert player.body[-1] == (5, 5)     # O corpo antigo (4,5) sumiu, o novo é (5,5)
