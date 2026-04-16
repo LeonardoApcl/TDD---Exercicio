926e84989611f22fd83cb7c19027a0b9c4efc6fc
Leonardo Apicelo Valente <leonardoapicelo.valente@gmail.com>
2026-04-16 01:39:06 -0300

    REFACTOR: Unificação da lógica para movimento
    como o código estava repetitivo, criei um dicionário para as direções e assim poder calcular qualquer direção 1 vez (sem precisar dos diversos if)

--- snake_control.py
+++ snake_control.py
@@ -3,20 +3,18 @@ class Snake:
         self.body = [(start_x, start_y), (start_x, start_y - 1)]
         self.direction = 'd'
 
+        self.directions = {
+            'w': (0, -1),
+            's': (0, 1),
+            'a': (-1, 0),
+            'd': (1, 0)
+        }
+
     def move(self, user_input, max_x, max_y):
-        if user_input == 'd':
-            new_head = (self.body[0][0] + 1, self.body[0][1])
-            self.body.insert(0, new_head)
-            self.body.pop()
-        if user_input == 'w':
-            new_head = (self.body[0][0], self.body[0][1] - 1)
-            self.body.insert(0, new_head)
-            self.body.pop()
-        if user_input == 's':
-            new_head = (self.body[0][0], self.body[0][1] + 1)
-            self.body.insert(0, new_head)
-            self.body.pop()
-        if user_input == 'a':
-            new_head = (self.body[0][0] - 1, self.body[0][1])
-            self.body.insert(0, new_head)
-            self.body.pop()
+        self.direction = user_input
+
+        head_x, head_y = self.body[0]
+        d_x, d_y = self.directions[self.direction]
+        new_head = (head_x + d_x, head_y + d_y)
+        self.body.insert(0, new_head)
+        self.body.pop()
