bed8a02f1c02ac318f85491aaa3c9aaee774ac2d
Leonardo Apicelo Valente <leonardoapicelo.valente@gmail.com>
2026-04-18 12:48:19 -0300

    REFACTOR: separei o teste RED
    achei melhor separar o teste de limite entre vertical e horizonta

--- test_snake.py
+++ test_snake.py
@@ -82,6 +82,8 @@ def test_snake_move_out_of_bounds():
     assert len(player.body) == 2         # O tamanho continua sendo 2
     assert player.body[-1] == (9, 5)     # A antiga cabeça (9,5) agora é o corpo
 
+#Teste (Red) - Testa comportamento de movimento para fora dos limites do mapa na vertical
+def test_snake_move_out_of_bounds_vertical():
     player = Snake(start_x=5, start_y=0) # Começa perto da borda superior do mapa
 
     player.move('w', max_x=10, max_y=10) # Tenta mandar para cima (na parede)
